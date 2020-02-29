6c79e6eccd4cb73f8123385de56c3c9c398332b6
Tomikland <tomikland@gmail.com>
2020-02-29 16:31:50 +0100

    Create DB from proto if needed

--- mysqlConnect.py
+++ mysqlConnect.py
@@ -6,6 +6,7 @@ db = mysql.connector.connect(
     passwd="",
 )
 
+#Does the db exist already?
 cursor = db.cursor()
 cursor.execute("SHOW DATABASES LIKE 'EUPRONET';")
 result = cursor.fetchall()
@@ -13,15 +14,24 @@ if(len(result) > 0):
     print("DB exists already.")
 else:
     print("Database doesn't exist yet. Creating...")
+    #Create from protodb.sql
     with open("protodb.sql", encoding="utf-8") as f:
-        sql = f.read()
-    cursor.execute(sql)
+        querycount = 0
+        for sql in f.read().split(";"):
+                if(querycount == 1): 
+                    #set cursor once db is created
+                    db.database = "EUPRONET"
+                    cursor = db.cursor()
+
+                cursor.execute(sql)
+                querycount += 1
+    
 
 db.database = "EUPRONET"
 cursor = db.cursor()
 
-#sql = "SELECT orszag, nepesseg FROM orszagok WHERE nepesseg > 160000 LIMIT 3"
-#cursor.execute(sql)
-#result = cursor.fetchall()
-#for entry in result:
-    #print(f"Entry: {entry}")
+sql = "SELECT orszag, nepesseg FROM orszagok WHERE nepesseg > 160000 LIMIT 3"
+cursor.execute(sql)
+result = cursor.fetchall()
+for entry in result:
+    print(f"Entry: {entry}")
