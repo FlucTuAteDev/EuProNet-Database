01c8083a2da61e43a6cd7cec1ef493519e6e6ee0
FlucTuAteDev <gucziadamlaszlo@gmail.com>
2020-02-29 13:40:49 +0100

    mysql connect

--- mysqlConnect.py
+++ mysqlConnect.py
@@ -1,12 +1,12 @@
 import mysql.connector
 
-myDb = mysql.connector.connect(
+db = mysql.connector.connect(
     host="localhost",
     user="root",
     passwd="",
     database="orszagok"
 )
 
-myCursor = myDb.cursor()
-myCursor.execute("SELECT * FROM orszagok WHERE nepesseg > 160000")
-myResult = myCursor.fetchall()
+cursor = db.cursor()
+cursor.execute("SELECT * FROM orszagok WHERE nepesseg > 160000")
+myResult = cursor.fetchall()
