68607f3205876a0fd4c06c98c470d329759d54c4
Tomikland <tomikland@gmail.com>
2020-02-29 15:42:10 +0100

    Create database from prototype if it doesn't exist yet

--- mysqlConnect.py
+++ mysqlConnect.py
@@ -4,9 +4,24 @@ db = mysql.connector.connect(
     host="localhost",
     user="root",
     passwd="",
-    database="orszagok"
 )
 
 cursor = db.cursor()
-cursor.execute("SELECT * FROM orszagok WHERE nepesseg > 160000")
+cursor.execute("SHOW DATABASES LIKE 'EUPRONET';")
 result = cursor.fetchall()
+if(len(result) > 0):
+    print("DB exists already.")
+else:
+    print("Database doesn't exist yet. Creating...")
+    with open("protodb.sql", encoding="utf-8") as f:
+        sql = f.read()
+    cursor.execute(sql)
+
+db.database = "EUPRONET"
+cursor = db.cursor()
+
+#sql = "SELECT orszag, nepesseg FROM orszagok WHERE nepesseg > 160000 LIMIT 3"
+#cursor.execute(sql)
+#result = cursor.fetchall()
+#for entry in result:
+    #print(f"Entry: {entry}")
---
+++ protodb.sql
@@ -0,0 +1,7 @@
+-- Prototype database definition for EUPRONET 4.0
+
+-- Current placeholder: http://www.infojegyzet.hu/webszerkesztes/mysql/orszagok/
+
+
+
+CREATE DATABASE IF NOT EXISTS `EUPRONET` DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
