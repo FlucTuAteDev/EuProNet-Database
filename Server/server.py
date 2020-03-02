edb0c6b1fbad1303f0b6dbcd8cf0eae4609f9863
FlucTuAteDev <gucziadamlaszlo@gmail.com>
2020-03-02 14:51:58 +0100

    Server optimization

--- Server/server.py
+++ Server/server.py
@@ -3,20 +3,19 @@ APIKEYVALUE = "wV9ysymCPn9yTYcilpIT"
 filename = "buttonPresses.csv"
 
 app = Flask(__name__)
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def result():
-    if (request.method == 'POST'):
-        apiKey = request.form['apiKey']
-        time = request.form['time']
-        buttons = request.form['buttons']
+    apiKey = request.form['apiKey']
+    time = request.form['time']
+    buttons = request.form['buttons']
 
-        if apiKey == APIKEYVALUE:
-            with open(filename, "a") as f:
-                f.write(f"{time};{buttons}\n")
-        
-        return "Written successfully!"
+    if apiKey == APIKEYVALUE:
+        with open(filename, "a") as f:
+            f.write(f"{time};{buttons}\n")
     else:
-        return "Not acceptable request"
+        return "APIs don't match!"
+    
+    return "Written successfully!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
