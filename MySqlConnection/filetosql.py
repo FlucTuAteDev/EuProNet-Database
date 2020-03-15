dcf8ba4e26669f37c21b664f0bdfd677d5e6c2fb
Tomikland <tomikland@gmail.com>
2020-03-15 14:50:46 +0100

    Login as 'HU' for now

--- MySqlConnection/filetosql.py
+++ MySqlConnection/filetosql.py
@@ -1,17 +1,50 @@
-path = "ardudata.txt"
-print(f"File: {path}")
-try:
-     with open(path, "x"):
-        print("\tFile doesn't exist, creating...")
-except:
-    print("\topening...")
+##File to sql: Uploads buffer file contents to database
 
-with open(path,"r+") as f:
-    if(f.readable()):
-        print(f"File content:\n {f.readlines()}" )
-    else:
-        print:"File could not be read."
+import mysql.connector
+import argparse
 
-    print("haloka", file=f)
-    f.seek(0)
-    print(f"File content after writing:\n {f.readlines()}" )
+dbname = "EUPRONET"
+protodb = r"MySqlConnection\protodb.sql"
+
+parser = argparse.ArgumentParser(description="Uploads buffer file contents to database")
+
+
+db = mysql.connector.connect(
+    host="176.241.15.209",
+    user="HU",
+    passwd="GkHfm0Sm5OZ6keqX",
+    database=dbname
+)
+
+cursor = db.cursor()
+# cursor.execute(f"SHOW DATABASES LIKE '{dbname}';")
+
+cursor.execute(f"SELECT CURRENT_USER();")
+result = cursor.fetchall()
+
+print(result)
+# if(len(result) > 0):
+#     print("DB exists already.")
+# else:
+#     print("Database doesn't exist yet. Creating...")
+    
+#     #Create from prototype
+#     with open(protodb, encoding="utf-8") as f:
+#         commands = f.read().split(";")
+#         for index, sql in enumerate(commands):
+#                 if(index == 1): 
+#                     #set cursor once db is created
+#                     db.database = dbname
+#                     cursor = db.cursor()
+
+#                 cursor.execute(sql)
+    
+
+# db.database = dbname
+# cursor = db.cursor()
+
+# sql = "SELECT orszag, nepesseg FROM orszagok WHERE nepesseg > 160000 LIMIT 3"
+# cursor.execute(sql)
+# result = cursor.fetchall()
+# for entry in result:
+#     print(f"Entry: {entry}")
