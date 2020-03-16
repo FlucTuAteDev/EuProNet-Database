5d61a89bb159f0ab71bdcef2845f9052c14b4794
FlucTuAteDev <gucziadamlaszlo@gmail.com>
2020-03-17 00:13:25 +0100

    Improved code

--- DataCollector/collector.py
+++ DataCollector/collector.py
@@ -1,93 +1,74 @@
 import argparse
 import re
-import networkCommunication
-import serialCommunication
+from pathlib import Path
 
-# Constant variables
-CFGFILE: str = "config.cfg"
-AVAILABLEMODES: list = ["network", "serial"]
-
-# Data storage
-data: dict = {}
-
-
-def listToRg(l: list):
+def choicesToRg(l: list):
     result: list = []
     for element in l:
         result.append(f"^{element}$")
     return '|'.join(result)
 
-
-# Checks if the configuration file defines the given element
-# with the either given or not given choices
-def cfgDefines(element: str, cfg: str = CFGFILE, regex: str = ".*"):
-    try:
-        with open(cfg, "r") as f:
-            for line in f.readlines():
-                (key, value) = line.split("=", 1)
-                if key == element and re.match(regex, value):
-                    break
-            else:
-                return False
-            return True
-    except:
-        return False
-
+# Constant variables
+CFGFILE: str = Path(f"{__file__}\..").absolute()/'config.cfg' #  Configuration file path
+AVAILABLE_MODES: list = ["network", "serial"]
+CONFIG_REQUIREMENTS = {
+    "filename": ".+",
+    "mode": choicesToRg(AVAILABLE_MODES),
+    "apikey": ".+",
+    "networkPort": "[0-9]{1,5}",
+    "serialPort": ".+"
+}
 
 def main():
-    # PUT DATA INTO CONFIG FILE #
+    #############################################
+    #   READ DATA AND PUT DATA INTO CONFIG FILE #
+    #############################################
+
+    # Configuration storage
+    cfg: dict = {}
+    # Set all the config values to None
+    for key in CONFIG_REQUIREMENTS.keys():
+        cfg[key] = None
+    try:
+        with open(CFGFILE, "r") as f:
+            # Reads file content in dictionary like form: [[a, b], [c, d]]
+            fileContent = [x.split("=", 1) for x in f.readlines()]
+            
+            # Read in the values from the config file if it's in the correct form
+            for content in fileContent:
+                try:
+                    key, value = [x.strip() for x in content]
+                    if key in CONFIG_REQUIREMENTS.keys() and value != "" and re.match(CONFIG_REQUIREMENTS[key], value):
+                        cfg[key] = value
+                except:
+                    pass
+    except:
+        pass
 
     # Create parser with arguments
     parser = argparse.ArgumentParser(
         description="Puts data from production line to a file"
     )
-    parser.add_argument(
-        "-f", "--filename",
-        required=not cfgDefines("filename")
-    )
-    parser.add_argument(
-        "-m", "--mode",
-        choices=AVAILABLEMODES,
-        required=not cfgDefines("mode", regex=listToRg(AVAILABLEMODES))
-    )
-    parser.add_argument(
-        "-k", "--apikey",
-        required=not cfgDefines("apikey")
-    )
-    parser.add_argument(
-        "-n", "--networkPort",
-        type=int
-    )
-    parser.add_argument(
-        "-s", "--serialPort"
-    )
-    
 
-    # If the config file exists then read its data to the data dictionary
-    try:
-        with open(CFGFILE, "r") as f:
-            fileContent = f.readlines()
-            for row in fileContent:
-                currentElement = row.split("=")
-                if (len(currentElement) == 2):
-                    data[currentElement[0]] = currentElement[1].strip()
-    # If it doesn't exist then create the file
-    except:
-        with open(CFGFILE, "x"):
-            pass
+    for key, value in cfg.items():
+        parser.add_argument(
+            f"-{key[0]}", f"--{key}",
+            required=(value == None)
+        )
 
     # Parse the arguments to a dictionary
     args = parser.parse_args()
     argsDict = vars(args)
 
     # Update the data dictionary with the given arguments
-    for k, v in argsDict.items():
-        if v is not None:
-            data[k] = v
+    for key, value in argsDict.items():
+        if value is not None and re.match(CONFIG_REQUIREMENTS[key], value):
+            cfg[key] = value
+            break
 
     # Also update the file with the given arguments
     with open(CFGFILE, "w") as f:
-        for k, v in data.items():
+        for k, v in cfg.items():
             f.write(f"{k}={v}\n")
 
 ##if __name__ == "__main__":
--- DataCollector/serialCommunication.py
+++ DataCollector/serialCommunication.py
@@ -4,12 +4,9 @@ import time
 def serialComm(**kwargs):
     with serial.Serial(kwargs) as ser:
         while 1:
+            # If there is incoming connection
             if ser.inWaiting() > 0:
-                serialData = ser.readline()
-                serialData = serialData.split("&")
+                # Write the recieved data to the given file
                 with open(kwargs["filename"], "a") as f:
-                    for data in serialData:
-                        dataParts = data.split("=")
-                        f.write(f"{dataParts[1]}")
-            else:
-                time.sleep(1)
+                    for data in ser.readline().split("&"):
+                        (key, value) = data.split("=")
