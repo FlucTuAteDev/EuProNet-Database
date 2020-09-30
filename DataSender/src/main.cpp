ee3bf2720f8dddfd3fb70b60d5499fcfb8d73dbf
Guczi Adam <gucziadamlaszlo@gmail.com>
2020-09-30 20:43:20 +0200

    Took out interrupts

--- DataSender/src/main.cpp
+++ DataSender/src/main.cpp
@@ -1,25 +1,23 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#define COLOR_COUNT 4 // Defines the number of color buttons present
-// Switch high / low for leds
-#define SWITCH 0x00
+#define BUTTON_COUNT 4 // Defines the number of buttons present
+#define SWITCH 0x01
 
-// Pins
-int finishButtonPin = D3;
-int colorButtonPins[] = {D5, D6, D7, D8};
-int ledPins[] = {D0, D1, D2, D4};
-
-// Button press logic variables
-enum State { None, Started, Discarded, Finished };
-State colorButtonStates[COLOR_COUNT] { None };
-volatile bool stateChanged = false;
-volatile int stateChIndex = -1;
+int okButtonPin = D2; // Pin of the ok button
+int colorButtonPins[] = {D5, D6, D7, D8}; // Defines the button pins
+int ledPins[] = {D0, D1, D3, D4};
 
 // Network details
 const char* SSID = "GucziFamily";
 const char* PASSWD = "Spiderma-6";
-const String APIKEY = "wV9ysymCPn9yTYcilpIT"; // Api key, to check whether the connection is authorized
+const String APIKEYVALUE = "wV9ysymCPn9yTYcilpIT"; // Api key, to check whether the connection is authorized
+
+// Button press logic variables
+enum State { None, Started, Discarded, Finished };
+bool wasPressed[BUTTON_COUNT] = { false };
+bool isPressed[BUTTON_COUNT] = { false };
+State colorButtonStates[BUTTON_COUNT] = { State::None };
 
 // HTTP connection variables
 String httpServer = "http://192.168.1.112:5000/"; // FLASK implementation
@@ -27,17 +25,22 @@ String payload;
 String httpResponseText;
 int httpResponseCode;
 
+// Serial communication variables
+String serialResponseText = "";
+
+// Helper functions
+
 // Send data through wifi
-void SendData(int color, int state)
+void SendData(int index)
 {
     HTTPClient http; // Declares this device as an HTTP client
     http.begin(httpServer); // Begins the connection with the specified server
     http.addHeader("Content-Type", "application/x-www-form-urlencoded"); // Defines the content type header
     // The values to be sent as a URL
     httpResponseText = 
-        "apikey=" + APIKEY + 
-        "&color=" + String(color) + 
-        "&state=" + String(state);
+        "apikey=" + APIKEYVALUE + 
+        "&color=" + String((index + 1)) +
+        "&state=" + String(colorButtonStates[index]);
 
     Serial.println(httpResponseText);
     httpResponseCode = http.POST(httpResponseText); // Sends the request with method POST
@@ -50,64 +53,56 @@ void SendData(int color, int state)
 }
 
 // Runs when a color button is pressed
-void ICACHE_RAM_ATTR colorInterrupt(void* pinp)
-{
-    // If the previous interrupt wasn't processed then don't proceed
-    if (stateChanged) return;
+// void ICACHE_RAM_ATTR colorInterrupt(void* pinp)
+// {
+//     // If the previous interrupt wasn't processed then don't proceed
+//     if (stateChanged) return;
 
-    // Get the GPIO pin from the pointer
-    int pin = *(int*)pinp;
+//     // Get the GPIO pin from the pointer
+//     int pin = *(int*)pinp;
 
-    // If there is a button with some state which is not the currently pressed one then don't proceed
-    for (int i = 0; i < COLOR_COUNT; i++)
-    {
-        if (colorButtonPins[i] != pin && colorButtonStates[i] != State::None)
-            return;
-    }
+//     // If there is a button with some state which is not the currently pressed one then don't proceed
+//     for (int i = 0; i < COLOR_COUNT; i++)
+//     {
+//         if (colorButtonPins[i] != pin && colorButtonStates[i] != State::None)
+//             return;
+//     }
 
-    // Search for the current button in the pins array
-    for (int i = 0; i < COLOR_COUNT; i++)
-    {
-        if (colorButtonPins[i] == pin)
-        {
-            // If the button doesn't have a state then set it to started and turn on the indicator led
-            if (colorButtonStates[i] == State::None)
-            {
-                colorButtonStates[i] = State::Started;
-                digitalWrite(ledPins[i], HIGH xor SWITCH);
-            }
-            // If its state is started then set it to discarded and turn off the indicator led
-            else if (colorButtonStates[i] == State::Started)
-            {
-                colorButtonStates[i] = State::Discarded;
-                digitalWrite(ledPins[i], LOW xor SWITCH);
-            }
-            stateChanged = true;
-            stateChIndex = i;
-            break;
-        }
-    }
-}
+//     // Search for the current button in the pins array
+//     for (int i = 0; i < COLOR_COUNT; i++)
+//     {
+//         if (colorButtonPins[i] == pin)
+//         {
+//             // If the button doesn't have a state then set it to started and turn on the indicator led
+//             if (colorButtonStates[i] == State::None)
+//             {
+//                 colorButtonStates[i] = State::Started;
+//                 digitalWrite(ledPins[i], HIGH xor SWITCH);
+//             }
+//             // If its state is started then set it to discarded and turn off the indicator led
+//             else if (colorButtonStates[i] == State::Started)
+//             {
+//                 colorButtonStates[i] = State::Discarded;
+//                 digitalWrite(ledPins[i], LOW xor SWITCH);
+//             }
+//             stateChanged = true;
+//             stateChIndex = i;
+//             break;
+//         }
+//     }
+// }
 
-// Runs when the finish button is pressed
-void ICACHE_RAM_ATTR finishInterrupt()
-{
-    // If the previous interrupt wasn't processed then don't proceed
-    if (stateChanged) return;
-
-    // Search for the pin with a started state
-    for (int i = 0; i < COLOR_COUNT; i++)
-    {
-        if (colorButtonStates[i] == State::Started)
-        {
-            // Set its state to finished and turn off the indicator led
-            colorButtonStates[i] = State::Finished;
-            stateChanged = true;
-            stateChIndex = i;
-            digitalWrite(ledPins[i], LOW xor SWITCH);
-        }
-    }
-}
+// // Send data through serial communication
+// void SendDataSerial()
+// {
+//     serialResponseText = 
+//         "apiKey=" + APIKEYVALUE + 
+//         "&buttons=" + getStringFromBool(colorButtonStates, BUTTON_COUNT) +
+//         "&discarded=" + getStringFromBool(discarded) +
+//         "&finished=" + getStringFromBool(okButtonState);
+    
+//     Serial.println(serialResponseText);
+// }
 
 void setup()
 {
@@ -120,31 +115,75 @@ void setup()
         delay(1000);
         Serial.print(".");
     }
-
     Serial.print("\nConnection established! IP address: ");
-    Serial.println(WiFi.localIP());
+    Serial.print(WiFi.localIP());
 
     // Initialize buttons and leds
-    for (int i = 0; i < COLOR_COUNT; i++)
+    for (int i = 0; i < BUTTON_COUNT; i++)
     {
+        pinMode(colorButtonPins[i], INPUT);
         pinMode(ledPins[i], OUTPUT);
         digitalWrite(ledPins[i], LOW xor SWITCH);
-        pinMode(colorButtonPins[i], INPUT);
-        attachInterruptArg(digitalPinToInterrupt(colorButtonPins[i]), colorInterrupt, &colorButtonPins[i], RISING);
     }
 
-    pinMode(finishButtonPin, INPUT);
-    attachInterrupt(digitalPinToInterrupt(finishButtonPin), finishInterrupt, RISING);
+    pinMode(okButtonPin, INPUT);
 }
 
+bool anyHasState(State array[])
+{
+    return std::any_of(std::begin(colorButtonStates), std::end(colorButtonStates), [](State n) { return n != State::None; });
+}
+
+int currentPressedIndex = -1;
 void loop()
 {
-    if (stateChanged)
+    // Goes through the color buttons available
+    for (size_t i = 0; i < BUTTON_COUNT; i++)
     {
-        SendData(stateChIndex + 1, colorButtonStates[stateChIndex]);
-        if (colorButtonStates[stateChIndex] != State::Started)
-            colorButtonStates[stateChIndex] = State::None;
-        stateChanged = false;
-        stateChIndex = -1;
+        isPressed[i] = digitalRead(colorButtonPins[i]) == HIGH;
+        //Serial.println(isPressed[i]);
+
+        if (!isPressed[i] && wasPressed[i]) // If the button was pressed and then released
+        {
+            // If any of the buttons have a state and it's not this one then break 
+            if (anyHasState(colorButtonStates) && colorButtonStates[i] == State::None)
+            {
+                wasPressed[i] = isPressed[i];
+                break;
+            }
+            
+            // If the pressed button had a started state change it to discarded
+            if (colorButtonStates[i] == State::Started)
+            {
+                colorButtonStates[i] = State::Discarded;
+                currentPressedIndex = -1;
+                digitalWrite(ledPins[i], LOW xor SWITCH);
+            }
+            // If it didn't set it to started
+            else
+            {
+                colorButtonStates[i] = State::Started;
+                currentPressedIndex = i;
+                digitalWrite(ledPins[i], HIGH xor SWITCH);
+            }
+
+            SendData(i);
+            // If the state was discarded reset it after sending the data
+            if (colorButtonStates[i] == State::Discarded) colorButtonStates[i] = State::None;
+        }
+        wasPressed[i] = isPressed[i];
     }
+
+    // If any of the buttons have a state and the ok button is pressed 
+    if (anyHasState(colorButtonStates) && digitalRead(okButtonPin) == HIGH)
+    {
+        // if (currentPressedIndex == -1) return;
+        colorButtonStates[currentPressedIndex] = State::Finished;
+        SendData(currentPressedIndex);
+        colorButtonStates[currentPressedIndex] = State::None;
+        digitalWrite(ledPins[currentPressedIndex], LOW xor SWITCH);
+        // for (int led : ledPins) digitalWrite(led, LOW xor SWITCH);
+    }
+    
+    delay(10);
 }
